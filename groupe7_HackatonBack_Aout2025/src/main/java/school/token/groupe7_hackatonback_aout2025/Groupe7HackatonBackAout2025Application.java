2bd320012ecde93bf4681f879bdfa5a0d47f06cb
DanyLachapelle <la228659@student.helha.be>
2025-07-08 18:30:47 +0200

    Initial commit BackEnd

---
+++ groupe7_HackatonBack_Aout2025/src/main/java/school/token/groupe7_hackatonback_aout2025/Groupe7HackatonBackAout2025Application.java
@@ -0,0 +1,13 @@
+package school.token.groupe7_hackatonback_aout2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Groupe7HackatonBackAout2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Groupe7HackatonBackAout2025Application.class, args);
+    }
+
+}
---
+++ groupe7_HackatonBack_Aout2025/src/test/java/school/token/groupe7_hackatonback_aout2025/Groupe7HackatonBackAout2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package school.token.groupe7_hackatonback_aout2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Groupe7HackatonBackAout2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
